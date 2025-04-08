dea7f6f33d9b82f7f168c59a76dc8ee5ff0335ae
i-soj-ng <jeong4530@gmail.com>
2025-04-08 15:51:44 +0900

    [feat]resolve confict

--- Project/team-project/src/App.vue
+++ Project/team-project/src/App.vue
@@ -5,6 +5,7 @@ import Header from "./components/Header.vue";
 
 <template>
   <Header />
+  <router-view></router-view>
 </template>
 
 <script></script>
---
+++ Project/team-project/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script>
+import LineGraph from "../components/LineGraph.vue";
+
+export default {
+  components: {
+    LineGraph,
+  },
+};
+</script>
+
+<template>
+  <main>
+    <line-graph />
+  </main>
+</template>

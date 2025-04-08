69c4e6d1937a52caae1f371e8d4b10404b52ee48
Baek HyeonBin <81628455+WhiteBin-bin@users.noreply.github.com>
2025-04-08 17:30:00 +0900

    view 수정

--- Project/team-project/src/views/TransactionsView.vue
+++ Project/team-project/src/views/TransactionsView.vue
@@ -1,13 +1,17 @@
 <script>
 import TransactionList from "../components/TransactionList.vue";
+import FilterBar from "../components/FilterBar.vue";
+
 
 export default {
   components: {
     TransactionList,
+    FilterBar
   },
 };
 </script>
 
 <template>
+  <filter-bar/>
   <transaction-list />
 </template>

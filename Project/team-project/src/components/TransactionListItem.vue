850c0f7dc7646657609b665d41f81206c8e4e5e4
Baek HyeonBin <81628455+WhiteBin-bin@users.noreply.github.com>
2025-04-09 19:55:43 +0900

    list 출력
    아직 한글로 변환 못함 해야됨

--- Project/team-project/src/components/TransactionListItem.vue
+++ Project/team-project/src/components/TransactionListItem.vue
@@ -1,81 +1,89 @@
 <template>
-    <tr class="border-bottom">
-      <td class="text-secondary">{{ transaction.date }}</td>
-      <td>
-        <span :class="['category-badge-rounded', transaction.type === 'income' ? 'text-success-category' : 'text-danger-category']">
-          {{ transaction.category }}
-        </span>
-      </td>
-      <td class="text-secondary">{{ transaction.description }}</td>
-      <td :class="transaction.type === 'income' ? 'text-success-amount' : 'text-danger-amount'">
-        {{ formatAmount(transaction.amount, transaction.type) }}
-      </td>
-      <td class="text-secondary">{{ transaction.memo || '-' }}</td>
-      <td class="text-center">
-        <button class="btn btn-sm btn-link text-primary p-0 me-2" @click="onEditTransaction">
-          <img src="@/assets/EDIT_BUTTON.svg" alt="수정" style="width: 1em; height: 1em; vertical-align: middle;">
-        </button>
-        <button class="btn btn-sm btn-link text-danger p-0" @click="onDeleteTransaction">
-          <img src="@/assets/BUTTON.svg" alt="삭제" style="width: 1em; height: 1em; vertical-align: middle;">
-        </button>
-      </td>
-    </tr>
-  </template>
-  
-  <script>
-  export default {
-    name: 'TransactionListItem',
-    props: {
-      transaction: {
-        type: Object,
-        required: true
-      }
-    },
-    methods: {
-      formatAmount(amount, type) {
-        const formattedAmount = amount.toLocaleString();
-        return type === 'income' ? `+${formattedAmount}원` : `-${formattedAmount}원`;
-      },
-      onEditTransaction() {
-        this.$emit('edit-transaction', {
-          type: this.transaction.type,
-          transaction: this.transaction
-        });
-      },
-      onDeleteTransaction() {
-        this.$emit('delete-transaction', {
-          type: this.transaction.type,
-          transaction: this.transaction
-        });
-      }
+  <tr class="border-bottom">
+    <td class="text-secondary">{{ transaction.date }}</td>
+    <td>
+      <span :class="['category-badge-rounded', transaction.type === 'income' ? 'text-success-category' : 'text-danger-category']">
+        {{ transaction.category }}
+      </span>
+    </td>
+    <td class="text-secondary">{{ transaction.description }}</td>
+    <td :class="transaction.type === 'income' ? 'text-success-amount' : 'text-danger-amount'">
+      {{ formatAmount(transaction.amount, transaction.type) }}
+    </td>
+    <td class="text-secondary">{{ transaction.memo || '-' }}</td>
+    <td class="text-center">
+      <button class="btn btn-sm btn-link text-primary p-0 me-2" @click="onEditTransaction">
+        <img src="@/assets/EDIT_BUTTON.svg" alt="수정" style="width: 1em; height: 1em; vertical-align: middle;">
+      </button>
+      <button class="btn btn-sm btn-link text-danger p-0" @click="onDeleteTransaction">
+        <img src="@/assets/BUTTON.svg" alt="삭제" style="width: 1em; height: 1em; vertical-align: middle;">
+      </button>
+    </td>
+  </tr>
+</template>
+
+<script>
+import { useTransactionStore } from '@/stores/transaction-store';
+
+export default {
+  name: 'TransactionListItem',
+  props: {
+    transaction: {
+      type: Object,
+      required: true
     }
-  };
-  </script>
-  
-  <style scoped>
-  .category-badge-rounded {
-    display: inline-block;
-    padding: 0.2em 0.6em;
-    font-size: 0.8em;
-    border-radius: 1rem;
-    white-space: nowrap;
+  },
+  setup() {
+    // Pinia store를 사용하기 위해 setup() 함수 내에서 호출
+    const transactionStore = useTransactionStore();
+    
+    return {
+      transactionStore
+    };
+  },
+  methods: {
+    formatAmount(amount, type) {
+      const formattedAmount = amount.toLocaleString();
+      return type === 'income' ? `+${formattedAmount}원` : `-${formattedAmount}원`;
+    },
+    onEditTransaction() {
+      // Pinia store의 데이터를 업데이트하고 싶다면 여기서 사용할 수 있습니다
+      this.$emit('edit-transaction', {
+        type: this.transaction.type,
+        transaction: this.transaction
+      });
+    },
+    onDeleteTransaction() {
+      // Pinia store의 데이터를 업데이트하고 싶다면 여기서 사용할 수 있습니다
+      this.$emit('delete-transaction', {
+        type: this.transaction.type,
+        transaction: this.transaction
+      });
+    }
   }
-  
-  .text-success-category {
-    color: #198754 !important;
-    background-color: #d1e7dd;
-  }
-  
-  .text-danger-category {
-    color: #dc3545 !important;
-    background-color: #f8d7da;
-  }
-  
-  .text-success-amount {
-    color: #198754 !important;
-  }
-  
-  .text-danger-amount {
-    color: #dc3545 !important;
-  }
-  </style>
+};
+</script>
+
+<style scoped>
+.category-badge-rounded {
+  display: inline-block;
+  padding: 0.2em 0.6em;
+  font-size: 0.8em;
+  border-radius: 1rem;
+  white-space: nowrap;
+}
+.text-success-category {
+  color: #198754 !important;
+  background-color: #d1e7dd;
+}
+.text-danger-category {
+  color: #dc3545 !important;
+  background-color: #f8d7da;
+}
+.text-success-amount {
+  color: #198754 !important;
+}
+.text-danger-amount {
+  color: #dc3545 !important;
+}
+</style>
